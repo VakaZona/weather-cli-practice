dc0f30dedcd66c7e1162a0a21d7014488f9daf88
Valery Zonov <valery.zonov@sibers.com>
2024-02-26 15:55:51 +0700

    add save file

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Token save');
+	} catch (error) {
+		printError(error.message)
+	}
+	
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
@@ -9,10 +20,11 @@ const initCLI = () => {
 		printHelp();
 	}
 	if (args.s) {
-		//save city
+		
 	}
 	if (args.t) {
-		// save token
+		return saveToken(args.t)
+
 	}
 	// weather
 }
